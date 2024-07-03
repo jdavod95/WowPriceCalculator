1276f8d6dae511fd95fdd37a31455d2e58294d1a
juhaszd@192.168.0.227 <jdavod95@gmail.com>
2024-07-03 13:42:05 +0200

    SaleService was missing

---
+++ wowStockCalculator/src/main/java/com/david/wowStockCalculator/services/SaleService.java
@@ -0,0 +1,23 @@
+package com.david.wowStockCalculator.services;
+
+import com.david.wowStockCalculator.domain.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SaleService {
+
+    Sale createSale(Long resourceId, Sale sale);
+
+    List<Sale> findAll();
+
+    Page<Sale> findAll(Pageable pageable);
+
+    Optional<Sale> findById(Long saleId);
+
+    boolean isExists(Long id);
+
+    void delete(Long id);
+}
