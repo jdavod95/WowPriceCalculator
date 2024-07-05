5f879742dd5e0e068226a1393b3dd8785a94594d
juhaszd@192.168.0.227 <jdavod95@gmail.com>
2024-07-05 11:28:40 +0200

    new sale, post functionality

--- wowStockCalculatorFrontend/src/app/app.module.ts
+++ wowStockCalculatorFrontend/src/app/app.module.ts
@@ -8,15 +8,19 @@ import { SaleService } from './services/sale.service';
 import { ResourcesComponent } from './components/resources/resources.component';
 import { SalesComponent } from './components/sales/sales.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SaleFormComponent } from './components/sale-form/sale-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResourcesComponent,
-    SalesComponent
+    SalesComponent,
+    SaleFormComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
